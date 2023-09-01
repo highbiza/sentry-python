67970a7ce8833124d4f13108986e687a4d1aea4c
Lars van de Kerkhof <lars@permanentmarkers.nl>
2023-09-01 09:24:13 +0200

    new version

--- sentry_sdk/consts.py
+++ sentry_sdk/consts.py
@@ -88,7 +88,7 @@ DEFAULT_OPTIONS = _get_default_options()
 del _get_default_options
 
 
-VERSION = "1.808.0"
+VERSION = "1.808.1"
 SDK_INFO = {
     "name": "sentry.python",
     "version": VERSION,
